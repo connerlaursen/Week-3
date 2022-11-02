cb60247095d5aaac0093f883ade88dee1278945b
Conner Laursen <conner@Conners-MacBook-Air.local>
2022-11-01 18:22:39 -0700

    added day 1 of week 3

---
+++ 04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,8 @@
+// Write Your JavaScript Code Here
+var personName = "Conner";
+var funFact = "I like Overwatch";
+var pets = "I have a black cat named Vega";
+
+console.log("My name is " + personName)
+console.log("Fun fact: " + funFact)
+console.log("My pet: " + pets)
---
+++ 06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,26 @@
+var one = 64;
+var two = "656302";
+var three = false;
+var four = 64.55;
+var five = "Howdy!";
+var six;
+
+// Insert comments to explain what each console log below will log to the console
+// one will log number, two will log string, three will log boolean, four will log number, five will log string, 6 will log undefined.
+
+console.log(typeof one); 
+console.log(typeof two); 
+console.log(typeof three); 
+console.log(typeof four); 
+console.log(typeof five);
+console.log(typeof six);
+
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+// four will log string, five will log boolean, six will log number
+console.log(typeof four);
+console.log(typeof five);
+console.log(typeof six);
